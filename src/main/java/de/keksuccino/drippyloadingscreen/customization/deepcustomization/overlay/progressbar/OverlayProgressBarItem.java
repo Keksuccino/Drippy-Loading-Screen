98adb810e8edefc1b4c053cd8c0b13e333f79e5d
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2023-02-07 00:56:47 +0100

    Release v2.1.1

---
+++ src/main/java/de/keksuccino/drippyloadingscreen/customization/deepcustomization/overlay/progressbar/OverlayProgressBarItem.java
@@ -0,0 +1,69 @@
+package de.keksuccino.drippyloadingscreen.customization.deepcustomization.overlay.progressbar;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.keksuccino.fancymenu.menu.fancy.menuhandler.deepcustomizationlayer.DeepCustomizationElement;
+import de.keksuccino.fancymenu.menu.fancy.menuhandler.deepcustomizationlayer.DeepCustomizationItem;
+import de.keksuccino.konkrete.properties.PropertiesSection;
+import de.keksuccino.konkrete.rendering.RenderUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.util.FastColor;
+import net.minecraft.util.Mth;
+
+import java.awt.*;
+
+public class OverlayProgressBarItem extends DeepCustomizationItem {
+
+    public String hexColorString = "#RRGGBB";
+    public Color hexColor = null;
+
+    public OverlayProgressBarItem(DeepCustomizationElement parentElement, PropertiesSection item) {
+
+        super(parentElement, item);
+
+        String hex = item.getEntryValue("custom_color_hex");
+        if ((hex != null) && !hex.toUpperCase().replace(" ", "").equals("#RRGGBB") && !hex.replace(" ", "").equals("")) {
+            Color c = RenderUtils.getColorFromHexString(hex);
+            if (c != null) {
+                this.hexColorString = hex;
+                this.hexColor = c;
+            }
+        }
+
+    }
+
+    @Override
+    public void render(PoseStack matrix, Screen menu) {
+
+        Minecraft mc = Minecraft.getInstance();
+        int i = mc.getWindow().getGuiScaledWidth();
+        double d1 = Math.min((double)mc.getWindow().getGuiScaledWidth() * 0.75D, mc.getWindow().getGuiScaledHeight()) * 0.25D;
+        double d0 = d1 * 4.0D;
+        int j1 = (int)(d0 * 0.5D);
+        int k1 = (int)((double)mc.getWindow().getGuiScaledHeight() * 0.8325D);
+        float currentProgress = 0.5F;
+
+        this.posX = i / 2 - j1;
+        this.posY = k1 - 5;
+        this.width = j1 * 2;
+        this.height = 10;
+
+        this.drawProgressBar(matrix, i / 2 - j1, k1 - 5, i / 2 + j1, k1 + 5, this.opacity, currentProgress);
+
+    }
+
+    private void drawProgressBar(PoseStack matrix, int i1, int i2, int i3, int i4, float opacity, float currentProgress) {
+        int i = Mth.ceil((float)(i3 - i1 - 2) * currentProgress);
+        int j = Math.round(opacity * 255.0F);
+        int k = FastColor.ARGB32.color(j, 255, 255, 255);
+        if (this.hexColor != null) {
+            k = FastColor.ARGB32.color(j, this.hexColor.getRed(), this.hexColor.getGreen(), this.hexColor.getBlue());
+        }
+        fill(matrix, i1 + 2, i2 + 2, i1 + i, i4 - 2, k);
+        fill(matrix, i1 + 1, i2, i3 - 1, i2 + 1, k);
+        fill(matrix, i1 + 1, i4, i3 - 1, i4 - 1, k);
+        fill(matrix, i1, i2, i1 + 1, i4, k);
+        fill(matrix, i3, i2, i3 - 1, i4, k);
+    }
+
+}
---
+++ src/main/java/de/keksuccino/drippyloadingscreen/mixin/mixins/client/MixinAnimationHandler.java
@@ -0,0 +1,21 @@
+package de.keksuccino.drippyloadingscreen.mixin.mixins.client;
+
+import de.keksuccino.drippyloadingscreen.customization.DrippyOverlayScreen;
+import de.keksuccino.fancymenu.menu.animation.AnimationHandler;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AnimationHandler.class)
+public class MixinAnimationHandler {
+
+    @Inject(at = @At("HEAD"), method = "isReady", cancellable = true, remap = false)
+    private static void onIsReady(CallbackInfoReturnable<Boolean> info) {
+        if ((Minecraft.getInstance().screen != null) && (Minecraft.getInstance().screen instanceof DrippyOverlayScreen)) {
+            info.setReturnValue(true);
+        }
+    }
+
+}
