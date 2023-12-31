c1ced6013cc8611bcfabaf22675b82e7e5c60812
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2023-12-31 10:47:13 +0100

    ---

--- common/src/main/java/de/keksuccino/drippyloadingscreen/customization/placeholders/Placeholders.java
+++ common/src/main/java/de/keksuccino/drippyloadingscreen/customization/placeholders/Placeholders.java
@@ -4,9 +4,11 @@ import de.keksuccino.fancymenu.customization.placeholder.PlaceholderRegistry;
 
 public class Placeholders {
 
+    public static final GameLoadingProgressPercentPlaceholder GAME_LOADING_PROGRESS_PERCENT = new GameLoadingProgressPercentPlaceholder();
+
     public static void registerAll() {
 
-        PlaceholderRegistry.register(new GameLoadingProgressPercentPlaceholder());
+        PlaceholderRegistry.register(GAME_LOADING_PROGRESS_PERCENT);
 
     }
 
