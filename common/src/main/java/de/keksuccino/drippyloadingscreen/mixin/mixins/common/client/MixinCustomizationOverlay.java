fd4464abfab7e68afcb52bb761b3ad21acc5be6c
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-02-18 12:24:23 +0100

    ---

--- common/src/main/java/de/keksuccino/drippyloadingscreen/mixin/mixins/common/client/MixinCustomizationOverlay.java
+++ common/src/main/java/de/keksuccino/drippyloadingscreen/mixin/mixins/common/client/MixinCustomizationOverlay.java
@@ -3,7 +3,7 @@ package de.keksuccino.drippyloadingscreen.mixin.mixins.common.client;
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.mojang.blaze3d.vertex.PoseStack;
 import de.keksuccino.fancymenu.customization.overlay.CustomizationOverlay;
-import de.keksuccino.fancymenu.util.rendering.ui.menubar.v2.MenuBar;
+import de.keksuccino.fancymenu.customization.overlay.CustomizationOverlayMenuBar;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.LoadingOverlay;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.At;
 public class MixinCustomizationOverlay {
 
     //Don't render FM's menu bar when DrippyOverlayScreen gets rendered in LoadingOverlay
-    @WrapWithCondition(method = "onRenderPost", at = @At(value = "INVOKE", target = "Lde/keksuccino/fancymenu/util/rendering/ui/menubar/v2/MenuBar;render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V"))
-    private boolean wrapMenuBarRenderingDrippy(MenuBar instance, PoseStack e, int leftX, int rightX, float c) {
+    @WrapWithCondition(method = "onRenderPost", at = @At(value = "INVOKE", target = "Lde/keksuccino/fancymenu/customization/overlay/CustomizationOverlayMenuBar;render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V"))
+    private boolean wrapMenuBarRenderingDrippy(CustomizationOverlayMenuBar instance, PoseStack graphics, int mouseX, int mouseY, float partial) {
         return !(Minecraft.getInstance().getOverlay() instanceof LoadingOverlay);
     }
 
