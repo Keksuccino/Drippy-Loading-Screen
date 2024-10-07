54af14dfbcab8ba37e137f11796fb7af3b0ece25
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-10-07 12:27:00 +0200

    Fixed workspace, because Gradle _sucks_

--- common/src/main/java/de/keksuccino/drippyloadingscreen/DrippyLoadingScreen.java
+++ common/src/main/java/de/keksuccino/drippyloadingscreen/DrippyLoadingScreen.java
@@ -19,7 +19,7 @@ public class DrippyLoadingScreen {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "3.0.1";
+	public static final String VERSION = "3.0.2";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "drippyloadingscreen";
 
